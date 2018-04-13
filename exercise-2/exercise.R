b4a04c27c292d851c7a305172ad87307f8875cdf
alan <alanaphayarath@yahoo.com>
2018-04-12 21:41:18 -0700

    Complete exercise 2 up until adding an average column

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -8,25 +8,25 @@
 library(fueleconomy)
 
 # Install and load the "dplyr" library
-
-
+install.packages("dplyr")
+library("dplyr")
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+manufactures <- select(vehicles, make)
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+distinct <- nrow(distinct(manufactures))
 
 # Filter the data set for vehicles manufactured in 1997
-
+cars_1997 <- filter(vehicles, year==1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
-
+sorted <- arrange(cars_1997, hwy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+mutated_cars_1997 <- mutate(cars_1997, average = (cty + hwy) / 2)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
